de89a5679396c55226f7c136df7690e00cabcc4a
Crowdbotics <team@crowdbotics.com>
2021-08-04 01:56:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "allinonestore_29435Identifier",
+  url: "https://allinonestore_29435Identifier.botics.co",
+  api: "https://allinonestore_29435Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
